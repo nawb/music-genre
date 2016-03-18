32573fd8734b4e0ea8eddead649f16de6baf2335
nawb <nabeel@zaeem.org>
2016-03-18 15:56:05 -0700

    Made a search function to test new songs

---
+++ search.py
@@ -0,0 +1,42 @@
+#!/usr/bin python
+'''
+' COEN 281
+' Team 9
+' This script requests user for song name, searches for and downloads its features to file.
+'''
+
+import sys, os, requests, json
+from pyechonest import song as sng
+from pyechonest import config
+config.ECHO_NEST_API_KEY = "PUWRUFW0PGBFUACZC"
+DEBUG = False
+OUTPUT_FILE = "_testcase"
+fh = open(OUTPUT_FILE, "w")
+
+##############
+## USER INPUT
+
+while not DEBUG:
+    songname = str(raw_input("\nSong name?? "))
+    ts_results = sng.search(title=songname)    
+    if ts_results:  #got some search results
+        print ("Found song by user "+ts_results[0].artist_name+" - "+ts_results[0].title)
+        resp = str(raw_input("Fetch this song? [y/n] "))
+        if "y" in resp.lower():
+            print("Fetching...")
+            break
+    else:
+        print "No such song found."
+
+############################
+## DOWNLOAD TIMBRE FEATURES
+
+ts = ts_results[0]
+#for aud_key, aud_val in ts.audio_summary.iteritems():
+#    print '    %-19s %s' % (aud_key, aud_val)
+audsum = requests.get(ts.audio_summary['analysis_url']).text
+for seg in json.loads(audsum)['segments']:
+    line = ",".join(map(str,seg['timbre'])) #convert each segment into comma-delimited line
+    fh.write(line+"\n")
+
+
